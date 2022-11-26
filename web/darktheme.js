c14ef05912a87ead7483b60bd1934b417a2c7000
Matteo Cavallaro <teoc98@users.noreply.github.com>
2022-11-26 12:32:49 +0100

    add dark theme toggle

--- web/darktheme.js
+++ web/darktheme.js
@@ -7,9 +7,12 @@ if (
 }
 
 function setTheme(theme) {
-  // document.documentElement.setAttribute("data-theme", theme);
+  document.documentElement.setAttribute("data-theme", theme);
   document.querySelector('html').style.filter = (theme === "dark" ? 'invert(100%)' : '');
-  document.querySelector("a[id=darkmodetoggle-link]").innertText = "Join the " + (theme === "dark" ? 'light' : 'dark') + " side";
+  toggle = document.querySelector("a[id=darkmodetoggle-link]");
+  if (toggle) {
+    toggle.innerText = "Join the " + (theme === "dark" ? 'light' : 'dark') + " side";
+  }
   // Set the user's theme preference to theme
   localStorage.setItem("theme", theme);
 }
@@ -24,6 +27,7 @@ if (currentTheme === "dark") {
   // Set the body data-theme attribute to match the local storage item
   setDark();
 }
+
 
 // Function that will switch the theme based on the if the theme toggle is checked or not
 function switchTheme() {
