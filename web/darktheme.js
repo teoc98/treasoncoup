05962a2ad16385114ce1b4a535173ee6340be794
Matteo <teoc98@github.com>
2022-11-26 12:30:13 +0100

    better darktheme

--- web/darktheme.js
+++ web/darktheme.js
@@ -6,27 +6,30 @@ if (
   localStorage.setItem("theme", "dark");
 }
 
+function setTheme(theme) {
+  // document.documentElement.setAttribute("data-theme", theme);
+  document.querySelector('html').style.filter = (theme === "dark" ? 'invert(100%)' : '');
+  document.querySelector("a[id=darkmodetoggle-link]").innertText = "Join the " + (theme === "dark" ? 'light' : 'dark') + " side";
+  // Set the user's theme preference to theme
+  localStorage.setItem("theme", theme);
+}
+function setDark()  { return setTheme("dark");  }
+function setLight() { return setTheme("light"); }
+
 // Get the theme toggle input
 const currentTheme = localStorage.getItem("theme"); // If the current local storage item can be found
 
 // Get the current theme from local storage
-if (currentTheme) {
+if (currentTheme === "dark") {
   // Set the body data-theme attribute to match the local storage item
-  // document.documentElement.setAttribute("data-theme", currentTheme);
-  document.querySelector('html').style.filter = 'invert(100%)';
+  setDark();
 }
 
 // Function that will switch the theme based on the if the theme toggle is checked or not
 function switchTheme() {
   if (localStorage.getItem("theme") === "dark") {
-    // document.documentElement.setAttribute("data-theme", "light");
-    document.querySelector('html').style.filter = '';
-    // Set the user's theme preference to dark
-    localStorage.setItem("theme", "light");
+    setLight();
   } else {
-    // document.documentElement.setAttribute("data-theme", "dark");
-    document.querySelector('html').style.filter = 'invert(100%)';
-    // Set the user's theme preference to light
-    localStorage.setItem("theme", "dark");
+    setDark();
   }
 }
