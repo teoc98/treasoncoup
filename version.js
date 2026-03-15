68a8e4c9506d02df5decf67248b5523d88396efd
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-03-15 17:19:11 +0100

    Refactor version.js to use async/await and use short commit as fallback

--- version.js
+++ version.js
@@ -1,26 +1,21 @@
 'use strict';
 
-var exec = require('child_process').exec;
+const { exec } = require('child_process');
+const { promisify } = require('util');
+const path = require('path');
 
-const server = require('path').basename(__dirname);
+const execAsync = promisify(exec);
+
+const server = "camelByte" // path.basename(__dirname);
 
 var contentPromise = null;
 
 // This is not used in production. Instead, the post-receive hook creates a static version.js.
 module.exports = function (req, res) {
-    // let content = `window.server = "${server}";`
-    let content = `window.server = "camelByte";`
+    let content = `window.server = "${server}";`
     if (contentPromise == null) {
         contentPromise = new Promise(function (resolve, reject) {
-            exec('git describe --tags --long', {cwd: __dirname}, function (err, stdout) {
-                if (err) {
-                    content += 'window.version = "";';
-                } else {
-                    var v = stdout.replace(/[\r\n]/g, '');
-                    content += `window.version = "${v}";`;
-                }
-                resolve(content);
-            });
+            getVersion().then(v => resolve(content + `window.version = "${v}";`));
         });
     }
 
@@ -29,3 +24,19 @@ module.exports = function (req, res) {
         res.send(content)
     });
 };
+
+async function getVersion() {
+    try {
+        const { stdout } = await execAsync('git describe --tags --long', { cwd: __dirname });
+        const version = stdout.replace(/[\r\n]/g, '');
+        return version;
+    } catch {
+        try {
+            const { stdout } = await execAsync('git rev-parse --short HEAD', { cwd: __dirname });
+            const shortCommit = stdout.replace(/[\r\n]/g, '');
+            return `commit ${shortCommit}`;
+        } catch {
+            return '';
+        }
+    }
+}
